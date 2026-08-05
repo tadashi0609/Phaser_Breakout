e4b806412af07fe0a065bde53b3e9c198cbdb2d4
tadashi0609 <tadashi0609w@gmail.com>
2026-08-05 13:25:42 +0900

    fix: ゲームリスタート時にブロックの表示が残っていたため、ブロックの初期化処理を変更

--- js/main.js
+++ js/main.js
@@ -96,6 +96,7 @@ class ExampleScene extends Phaser.Scene {
 		);
 
 		this.initGame();
+		this.initBricks();
 	}
 	update() {
 		this.physics.collide(this.ball, this.paddle, (ball, paddle) =>
@@ -114,7 +115,10 @@ class ExampleScene extends Phaser.Scene {
 			this.ball.getBounds()
 		);
 
+		// アクティブなブロックがない = ステージクリア
 		if (this.bricks.countActive() === 0) {
+			this.bricks.destroy(true);
+			this.initBricks();
 			this.initGame();
 		}
 
@@ -127,8 +131,11 @@ class ExampleScene extends Phaser.Scene {
 		if (this.ball) {
 			this.ball.destroy();
 		}
-		if (this.bricks) {
-			this.bricks.destroy();
+
+		if (this.bricks && this.bricks.countActive(false) !== 0) {
+			this.bricks.children.iterate(brick => {
+				brick.enableBody(false, 0, 0, true, true);
+			})
 		}
 
 		this.startButton.visible = true;
@@ -138,7 +145,6 @@ class ExampleScene extends Phaser.Scene {
 		this.paddle.x = this.scale.width / 2;
 
 		this.initBall();
-		this.initBricks();
 
 		this.scoreText.setText('Points: ' + this.score);
 		this.livesText.setText('Lives: ' + this.lives);
@@ -183,8 +189,7 @@ class ExampleScene extends Phaser.Scene {
 				const brickX = c * (bricksLayout.width + bricksLayout.padding) + bricksLayout.offset.left;
 				const brickY = r * (bricksLayout.height + bricksLayout.padding) + bricksLayout.offset.top;
 
-				const newBrick = this.add.sprite(brickX, brickY, 'brick');
-				this.physics.add.existing(newBrick);
+				const newBrick = this.physics.add.sprite(brickX, brickY, 'brick');
 				newBrick.body.setImmovable(true);
 				this.bricks.add(newBrick);
 			}
@@ -197,20 +202,7 @@ class ExampleScene extends Phaser.Scene {
 	}
 
 	hitBrick(ball, brick) {
-		const destroyTween = this.tweens.add({
-			targets: brick,
-			ease: 'Linear',
-			repeat: 0,
-			duration: 200,
-			props: {
-				scaleX: 0,
-				scaleY: 0,
-			},
-			onComplete() {
-				brick.destroy();
-			},
-		});
-		destroyTween.play();
+		brick.disableBody(true, true);
 
 		this.score += 10;
 		this.scoreText.setText('Points: ' + this.score);
